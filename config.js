3e4aa0ae61750879b2b8526623e9ed307510dc00
Daniel Lando <daniel.sorridi@gmail.com>
2020-06-05 10:37:38 +0200

    persistence/mqemitter support

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  protos: ['tcp'],
+  host: '127.0.0.1',
+  port: 1883,
+  wsPort: 3000,
+  wssPort: 4000,
+  tlsPort: 8883,
+  brokerId: 'aedes-cli',
+  persistence: {
+    name: 'mongodb',
+    options: {
+      url: 'mongodb://127.0.0.1/aedes'
+    }
+  },
+  //   mq: {
+  //     name: 'mongodb',
+  //     options: {
+  //       url: 'mongodb://127.0.0.1/aedes'
+  //     }
+  //   },
+  //   persistence: {
+  //     name: 'redis',
+  //     options: {}
+  //   },
+  //   mq: {
+  //     name: 'redis',
+  //     options: {}
+  //   }
+  key: null,
+  cert: null,
+  rejectUnauthorized: true,
+  verbose: false,
+  veryVerbose: false,
+  noPretty: false
+}
