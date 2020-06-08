c183de43de83a51c3c8e5a8181200d2c14820c35
Daniel Lando <daniel.sorridi@gmail.com>
2020-06-08 11:55:45 +0200

    test multiple servers

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,9 +3,12 @@ const { start, stop } = require('./helper')
 
 const defaults = require('../config')
 
+const { join } = require('path')
+
 require('leaked-handles')
 
 test('start without args', async function (t) {
+  t.plan(7)
   var setup = await start()
 
   t.tearDown(stop.bind(t, setup))
@@ -14,4 +17,34 @@ test('start without args', async function (t) {
   var info = setup.servers[0].address()
   t.equal(info.address, defaults.host, 'should have default host')
   t.equal(info.port, defaults.port, 'should have default port')
+
+  var broker = setup.broker
+  t.equal(broker.id, defaults.brokerId, 'should have default brokerId')
+  t.equal(broker.queueLimit, defaults.queueLimit, 'should have default queueLimit')
+  t.equal(broker.maxClientsIdLength, defaults.maxClientsIdLength, 'should have default maxClientsIdLength')
+  t.equal(broker.connectTimeout, defaults.connectTimeout, 'should have default connectTimeout')
+})
+
+test('start multiple servers', async function (t) {
+  t.plan(9)
+
+  var servers = {
+    tcp: defaults.port,
+    ws: defaults.wsPort,
+    tls: defaults.tlsPort,
+    wss: defaults.wssPort
+  }
+  var args = ['--protos', Object.keys(servers).join(','), '--cert', join(__dirname, '/secure/server.cert'), '--key', join(__dirname, '/secure/server.key')]
+
+  var setup = await start(args)
+
+  t.tearDown(stop.bind(t, setup))
+
+  t.equal(setup.servers.length, 4, 'should start 4 server')
+
+  for (const server of setup.servers) {
+    var info = server.address()
+    t.equal(info.address, defaults.host, 'should have default host')
+    t.equal(info.port, servers[server._protocol], 'should have default port')
+  }
 })
