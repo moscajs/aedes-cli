d4d4318f57b519e4d44d36db8b241079e0dbf277
Daniel Lando <daniel.sorridi@gmail.com>
2020-06-09 08:18:39 +0200

    removed custom promisify

--- test/authorizer.test.js
+++ test/authorizer.test.js
@@ -2,19 +2,7 @@ const { test } = require('tap')
 
 const Authorizer = require('../lib/authorizer')
 
-const promisifyMulti = function (fn) {
-  return function (...args) {
-    return new Promise((resolve, reject) => {
-      fn(...args, function (...res) {
-        if (res[0]) reject(res[0])
-        else {
-          res.shift()
-          resolve(res)
-        }
-      })
-    })
-  }
-}
+const { promisify } = require('util')
 
 test('add user and authenticate/authorize', async function (t) {
   t.plan(7)
@@ -30,26 +18,26 @@ test('add user and authenticate/authorize', async function (t) {
 
   t.notEqual(authorizer.users[username], undefined, 'should add user')
 
-  var authenticate = promisifyMulti(authorizer.authenticate())
+  var authenticate = promisify(authorizer.authenticate())
 
   var res = await authenticate(client, username, password)
 
-  t.equal(res[0], true, 'should successfully authenticate')
+  t.equal(res, true, 'should successfully authenticate')
 
-  var authorizePub = promisifyMulti(authorizer.authorizePublish())
-  var authorizeSub = promisifyMulti(authorizer.authorizeSubscribe())
+  var authorizePub = promisify(authorizer.authorizePublish())
+  var authorizeSub = promisify(authorizer.authorizeSubscribe())
 
   res = await authorizePub(client, 'not/allowed', null)
-  t.equal(res[0], false, 'should not authorize pub on not allowed topics')
+  t.equal(res, false, 'should not authorize pub on not allowed topics')
 
   res = await authorizePub(client, 'allowed/topic/1', null)
-  t.equal(res[0], true, 'should authorize pub on allowed topics')
+  t.equal(res, true, 'should authorize pub on allowed topics')
 
   res = await authorizeSub(client, 'not/allowed')
-  t.equal(res[0], false, 'should not authorize sub on not allowed topics')
+  t.equal(res, false, 'should not authorize sub on not allowed topics')
 
   res = await authorizeSub(client, 'allowed/topic/1')
-  t.equal(res[0], true, 'should authorize sub on allowed topics')
+  t.equal(res, true, 'should authorize sub on allowed topics')
 
   authorizer.rmUser(username)
 
