c962c25e3ca006061484aa71d5a4e89d35c9bc54
Daniel Lando <daniel.sorridi@gmail.com>
2020-06-11 16:59:06 +0200

    docs: fix docker-compose example with config

---
+++ docker/dockerConfig.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // SERVERS
+  protos: ['tcp'],
+  host: '127.0.0.1',
+  port: 1883,
+  wsPort: 3000,
+  wssPort: 4000,
+  tlsPort: 8883,
+  key: null,
+  cert: null,
+  rejectUnauthorized: true,
+  // AUTHORIZER
+  credentials: './credentials.json',
+  // AEDES
+  brokerId: 'aedes-cli',
+  concurrency: 100,
+  queueLimit: 42,
+  maxClientsIdLength: 23,
+  heartbeatInterval: 60000,
+  connectTimeout: 30000,
+  disableStats: true,
+  // PERSISTENCES
+  persistence: {
+    name: 'mongodb',
+    options: {
+      url: 'mongodb://mongo/aedes'
+    }
+  },
+  mq: {
+    name: 'mongodb',
+    options: {
+      url: 'mongodb://mongo/aedes'
+    }
+  },
+  // LOGGER
+  verbose: false,
+  veryVerbose: false,
+  noPretty: false
+}
