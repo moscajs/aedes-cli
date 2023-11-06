8976a609a3113f6ff1126690dfd73519c1cd619e
Daniel Lando <daniel.sorridi@gmail.com>
2023-11-06 14:54:00 +0100

    chore: add log on persistence test

--- test/persistence.test.js
+++ test/persistence.test.js
@@ -5,6 +5,8 @@ const { join } = require('path')
 
 const PERSISTENCE = process.env.DB || 'mongodb'
 
+console.log(`Testing ${PERSISTENCE} persistence`)
+
 test(`test ${PERSISTENCE} persistence from config`, async function (t) {
   const persistenceConfig = join(__dirname, `config/${PERSISTENCE}Config.js`)
 
